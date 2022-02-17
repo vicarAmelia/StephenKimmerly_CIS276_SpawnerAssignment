98fdfa7b554c53ec93c06e856ac16db7a224178a
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-02-17 14:38:18 -0500

    Bunch of testing and updates

---
+++ Spawner Assignment/Assets/ObjectPlacementManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacementManager : MonoBehaviour
+{
+    //assigned in inspector
+    public Transform placeableObject;
+    public Camera cam;
+    public LayerMask mask;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity, mask))
+        {
+            placeableObject.position = hitInfo.point + new Vector3(0, 0.5f, 0);
+            placeableObject.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+
+    }
+}
