e88eef68e7e4c66319914154bd498de970f35d79
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-02-17 15:50:41 -0500

    Fixed Object Placement Script

---
+++ Spawner Assignment/Assets/Scripts/ObjectPlacementManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacementManager : MonoBehaviour
+{
+    public Camera cam;
+    public Transform placeableObject;
+    public LayerMask mask;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity, mask))
+        {
+            placeableObject.position = hitInfo.point + new Vector3(0, 0.5f, 0);
+            placeableObject.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+}
